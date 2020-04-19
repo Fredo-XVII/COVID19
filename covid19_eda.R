5b314afd285e8755bd50e2d55055a5a8335ed713
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-04-19 14:43:36 -0500

    have minnesota data

--- covid19_eda.R
+++ covid19_eda.R
@@ -7,10 +7,17 @@ library(sf)
 
 # Load data
 
-cnty_data <- R.COVID.19::us_geo_confirmed_daily()
+cnty_data_shp <- R.COVID.19::pub_hlth_status_by_cnty_shp()
 
-cnty_data %>% #View()
+cnty_data <- R.COVID.19::us_geo_confirmed_daily() %>% 
+  dplyr::mutate(fips = forcats::as_factor(FIPS))
+
+
+cnty_shp <- cnty_data %>% 
+  dplyr::left_join(cnty_data_shp, by = c("fips" = "fips"))
+
+cnty_shp %>% #View()
   dplyr::filter(Province_State %in% c("Minnesota")) %>%
   View()
 
-length(unique(st_data$FIPS))
+length(unique(cnty_shp$fips))
