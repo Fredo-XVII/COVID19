c40cf82e8c4e6e9b72ae167c8f52888805a5a0d4
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-04-26 19:15:51 -0500

    testing animate

--- covid19_by_time.R
+++ covid19_by_time.R
@@ -4,6 +4,7 @@
 library(R.COVID.19)
 library(tidyverse)
 library(sf)
+library(gganimate)
 
 # Get Covid19 Data
 tidycensus::fips_codes %>% View()
@@ -17,13 +18,28 @@ cnty_data <- R.COVID.19::us_geo_confirmed_daily() %>%
 cnty_data_shp <- cnty_data %>% 
   dplyr::inner_join(cnty_shp, by = c("fips" = "GEOID"))
 
-cnty_data_shp %>% head() %>% View()
+cnty_data_shp %>% tail() %>% View()
 
 cnty_data_shp$greg_d <- as.Date(cnty_data_shp$greg_d, format = '%m/%d/%y')
 
-cnty_data_shp %>% 
+cnty_data_shp <- cnty_data_shp %>% 
+  dplyr::mutate(daily_delta = confirmed_cases - lag(confirmed_cases))
+
+cnty_data_shp <- sf::st_as_sf(cnty_data_shp)
+
+map_base <- cnty_data_shp %>% #filter(greg_d == as.Date('2020-04-24')) %>% 
   filter(Province_State == "Minnesota") %>% 
-  ggplot()
+  ggplot(aes(fill = log2(.data$daily_delta), frame = greg_d)) +
+  geom_sf(color = NA) +
+  coord_sf(crs = "WGS84") + 
+  scale_fill_viridis_c(option = "plasma") 
+
+map_animate <- map_base +
+  gganimate::view_follow()
+
+animate(map_animate,end_pause=25, nframes=85,fps=1)
+save_animation(last_animation(), file="./test_animation.gif")
+gganimate::an
 
 #gganimate example
 a3 <- 
@@ -40,4 +56,4 @@ a3 <-
 
 
 animate(a3,end_pause=25, nframes=350,fps=12)
-save_animation(last_animation(), file="PATH_FOR_GIF3")
+save_animation(gganimate::last_animation(), file="PATH_FOR_GIF3")
